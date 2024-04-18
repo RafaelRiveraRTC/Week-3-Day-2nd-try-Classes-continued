78a0a735aaac0e21032c1002149f5c730fa0c3da
Tetiana343 <emilianovolk75@gmail.com>
2024-04-17 17:53:09 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Week_3_Day_2nd_try_Classes_continued
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
